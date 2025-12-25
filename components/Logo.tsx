58936ead2745f21fb42086c8199d526fcca2872a
Thierno Bocar DIENG <tdieng591@gmail.com>
2025-12-25 13:32:44 +0000

    MAJ logo V2

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps {
 }
 
 export const Logo: React.FC<LogoProps> = ({ className = "h-16", withText = true }) => {
-  const logoUrl = "https://media.licdn.com/dms/image/v2/D4E0BAQEDRW1wrkNA2g/company-logo_200_200/B4EZn_LZ1.GoAI-/0/1760922804191/ecliptix_group_logo?e=1766620800&v=beta&t=OGsYnzsuE3yGLA6ETAM_rzujDpAqoM2_kEmwBpr8Q44";
+  const logoUrl = "https://media.licdn.com/dms/image/v2/D4E0BAQEDRW1wrkNA2g/company-logo_200_200/B4EZn_LZ1.GoAI-/0/1760922804191/ecliptix_group_logo?e=1768435200&v=beta&t=xtgbP851Y9A0SavAuXy4a7BhX8UgyVXsSFuy3poYyFE";
 
   return (
     <div className={`flex items-center gap-4 ${className}`}>
